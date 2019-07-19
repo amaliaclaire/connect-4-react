89cdaafbd93214b7c3f7e39495e1e2c0311f99c7
Amalia <amalia.fergin@gmail.com>
2019-07-19 01:05:26 -0700

    Add reset button

--- src/GameLogic.js
+++ src/GameLogic.js
@@ -12,65 +12,74 @@ export default class GameLogic {
     this._height = height;
 
     this._activePlayer = 0;
+    this._inProgress = true;
 
+    this._resetBoard();
+  }
+
+  _resetBoard() {
     this._boardState = [];
-    for (let row = 0; row < height; row++) {
+    for (let row = 0; row < this._height; row++) {
       this._boardState[row] = [];
-      for (let col = 0; col < width; col++) {
+      for (let col = 0; col < this._width; col++) {
         this._boardState[row][col] = null;
       }
     }
   }
 
-get width () {
-  return this._width;
-}
+  get width() {
+    return this._width;
+  }
+
+  get inProgress() {
+    return this._inProgress;
+  }
 
   get boardState() {
     return this._boardState;
   }
 
-  drop (col) {
+  drop(col) {
     for (let row=this._height - 1; row>=0; row--) {
       if (this._boardState[row][col] === null) {
         this._setCell(col, row, this._activePlayer);
         const winningLine = this._checkForWin(this._activePlayer);
 
-
         if (winningLine) {
-
           alert(`player ${this._activePlayer} has won`);
-
-
-
-          return
+          this._inProgress = false;
+          return;
         }
 
         this._activePlayer = this._activePlayer ? 0 : 1;
-        return true;
-
-
+        return;
       }
     }
   }
-   _checkForWin(color) {
-      // from each cell in the grid, we check for 4 in a row for each direction aka horizontal, diagonal, vertical
-      // x & y are coordinate axis
-      for (let y = 0; y < this._height; y++) {
-        for (let x = 0; x < this._width; x++) {
-          for (let direction of GameLogic.DIRECTIONS) {
-            // console.log(direction);
-            if (this._checkFourColors(color, x, y, direction)) {
-              return {
-                x: x,
-                y: y,
-                direction: direction
-              }
+
+  reset() {
+    this._inProgress = true;
+    this._resetBoard();
+  }
+
+  _checkForWin(color) {
+    // from each cell in the grid, we check for 4 in a row for each direction aka horizontal, diagonal, vertical
+    // x & y are coordinate axis
+    for (let y = 0; y < this._height; y++) {
+      for (let x = 0; x < this._width; x++) {
+        for (let direction of GameLogic.DIRECTIONS) {
+          // console.log(direction);
+          if (this._checkFourColors(color, x, y, direction)) {
+            return {
+              x: x,
+              y: y,
+              direction: direction
             }
           }
         }
       }
-      return null;
+    }
+    return null;
   }
 
   // when you see "i" use direction.i and direction.j
@@ -89,7 +98,7 @@ get width () {
         break;
       }
 
-      if(this._boardState[checkY][checkX] != color) {
+      if(this._boardState[checkY][checkX] !== color) {
         win = false;
         break;
       }
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -5,7 +5,10 @@ import '../index.css';
 export default class GameBoard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {board: this.props.logic.boardState};
+    this.state = {
+      board: this.props.logic.boardState,
+      inProgress: this.props.logic.inProgress
+    };
   }
 
   render() {
@@ -13,34 +16,48 @@ export default class GameBoard extends React.Component {
 
     const buttons = [];
     for (let i = 0; i < logic.width; i++) {
-      buttons.push(<th><button class="myBtn" onClick={e => this.drop(logic, i)}>Drop</button></th>)
+      buttons.push(<th><button class="myBtn" disabled={!this.state.inProgress} onClick={e => this.drop(logic, i)}>Drop</button></th>)
     }
 
 
     return (
-      <table className="board">
-        <tr>
-          {buttons}
+      <div>
+        <table className="board">
+          <tr>
+            {buttons}
+          </tr>
+          {
+            this.state.board.map((row, index) => (
+              <tr key={index}>
+                {
+                  row.map((cell, index) =>
+                    <td  className={cell === null ? 'empty' : `player${cell}`} key={index}>
+                    </td>
+                  )
+                }
+              </tr>
+            ))
+          }
+        </table>
 
-        </tr>
-        {
-          this.state.board.map((row, index) => (
-            <tr key={index}>
-              {
-                row.map((cell, index) =>
-                  <td  className={cell === null ? 'empty' : `player${cell}`} key={index}>
-                  </td>
-                )
-              }
-            </tr>
-          ))
-        }
-      </table>
+        <button onClick={e => this.reset(logic)}>Reset</button>
+      </div>
     );
   }
 
   drop(logic, i) {
     logic.drop(i);
-    this.setState({board: logic.boardState});
+    this.setState({
+      board: logic.boardState,
+      inProgress: this.props.logic.inProgress
+    });
+  }
+
+  reset(logic) {
+    logic.reset();
+    this.setState({
+      board: logic.boardState,
+      inProgress: logic.inProgress
+    });
   }
 }
